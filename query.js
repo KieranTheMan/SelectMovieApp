94adeb76238048c18416d3628f0cd7c86fe149ec
KieranTheMan <kieran9fowler@gmail.com>
2024-05-16 09:57:35 +0100

    add OpenAI getChatCompletion function

--- query.js
+++ query.js
@@ -10,6 +10,7 @@ export default app.get('/query',async(req,res) => {
     async function main(input) {
         const embedding = await createEmbedding(input)
         const match = await findNearestMatch(embedding)
+        await getChatCompletion(match, input);
     }
 
         async function createEmbedding(input) {
@@ -29,5 +30,27 @@ export default app.get('/query',async(req,res) => {
             const match = data.map(obj => obj.content).join();
             return match
         }
+
+         // Use OpenAI to make the response conversational
+         const chatMessages = [{
+            role: 'system',
+            content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.` 
+        }];
+
+        async function getChatCompletion(text, query) {
+            chatMessages.push({
+              role: 'user',
+              content: `Context: ${text} Question: ${query}`
+            });
+            
+            const response = await openai.chat.completions.create({
+              model: 'gpt-4',
+              messages: chatMessages,
+              temperature: 0.5,
+              frequency_penalty: 0.5
+            });
+            console.log(response.choices[0].message.content);
+        }
+
         main(query);
 });
