326a1ecf97356e910b58407b4a19966bd9b06bf3
KieranTheMan <kieran9fowler@gmail.com>
2024-05-23 10:15:19 +0100

    improve system prompting, changed findNearstMatch to a single output, add search veribal to POST res

--- query.js
+++ query.js
@@ -18,8 +18,8 @@ export default app.post('/userquery',async(req,res) => {
     
     async function main(input) {
         const embedding = await createEmbedding(input);
-        const {match, title} = await findNearestMatch(embedding);
-        await getCompletionAndImage(match, input, title)
+        const match = await findNearestMatch(embedding);
+        await getCompletionAndImage(match, input)
     }
         async function createEmbedding(input) {
             const embeddingResponse = await openai.embeddings.create({
@@ -35,25 +35,23 @@ export default app.post('/userquery',async(req,res) => {
                 match_threshold: 0.50,
                 match_count: 1
             });
-            const output1 = data.map(obj=> obj.content)
-            const output2 = data.map(obj=> obj.content)
-            return {
-                title: output1[0],
-                match: output2
-            }
+
+            const output = data.map(obj=> obj.content)
+            console.log(`MatchOutPUT${output}`)
+            return output;
         }
         
          //Use OpenAI to make the response conversational and inlude MovieDB Api to Result
          const chatMessages = [{
             role: 'system',
-            content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.` 
+            content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given some information on the context about movies and answers from the customer. Your main job is to formulate a short answer using the context given and answer recived from the following questions: what is your favourite movie and why?, Are you in the mood for a classic or new movie?, Do you want a fun or serious movie? If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.`
         }];
 
-        async function getCompletionAndImage(text, query, image) {
+        async function getCompletionAndImage(text, querys) {
             try{
                 chatMessages.push({
                 role: 'user',
-                content: `Context: ${text} Question: ${query}`
+                content: `Context: ${text} Answer: ${querys}`
                 });
                 
                 const response = await openai.chat.completions.create({
@@ -63,12 +61,21 @@ export default app.post('/userquery',async(req,res) => {
                 frequency_penalty: 0.5
                 });
                 
-                let search = image.slice(0,30)
-                const parameters = {query: search}
+                let search = text.slice(0, 30);
+                console.log(`SEARCH ${search}`)
+                const parameters = {
+                    query:search
+                }
                 const SearchRes = await moviedb.searchMovie(parameters)
-                const listofmovies = SearchRes.results.map(obj=> obj.poster_path)[0];
-
-                res.json({image: listofmovies, message: response.choices[0].message.content})
+                const images = SearchRes.results.map(obj=> obj.poster_path)[0];
+                     
+                console.log(`iMAGE RESULTS${images}`)
+                res.json(
+                {
+                    title: search,
+                    image: images,
+                    message: response.choices[0].message.content
+                })
                 
             } catch (error) {
                 return error
