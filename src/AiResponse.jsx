17497557d37133605095b3e1d4469369573ab82c
KieranTheMan <kieran9fowler@gmail.com>
2024-06-04 10:05:28 +0100

    AiResponse: add useEffect and state for custom loader, setTimeout to 6000, use visable-content & hidden class to hide ai title, image, message

--- src/AiResponse.jsx
+++ src/AiResponse.jsx
@@ -1,25 +1,43 @@
 import { useNavigate } from 'react-router-dom'
 import './Style.css'
 import logo from '/MovieSelektLogo.svg'
+import { useEffect, useState } from 'react';
+
+
+
+
 
 function AiResponse() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(()=>{
+      setLoading(false)
+    },6000)
+  }, []);
+
 const navigate = useNavigate();
   return (
     <>
-      <div className='background'>
-        <div>
-            <img id='logo' src={logo} alt="movie logo" />
-        </div>
-            <div>
-                <div id='aiimage'/>
-                <p id='aimessage'></p>
-            </div>
-                  <div style={{paddingTop: 20 }}>
-                    <button className='button-53' onClick={() => navigate('/')}>
-                        Go Again
-                    </button>
-                  </div>
-      </div>
+      
+      
+                <div className='ex-userInterfaceBG'>
+                  
+                  {loading && <div className="custom-loader"></div>}
+            <div className={`visable-content ${loading ? 'hidden' : ''}`}>
+                          <div id='aiTitle'></div>
+                          <div id='aiimage'/>
+                          <p id='aimessage'></p>
+                      
+                          <button  className='button-53' onClick={() => navigate('/')}>
+                                  Go Again
+                              </button>
+                      
+                      </div>
+                      
+                     
+                      
+          </div>
     </>
   )
 }
