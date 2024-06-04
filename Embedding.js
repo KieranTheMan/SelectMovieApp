b139ca2427918474c12c9de3cc097549b13c38bb
KieranTheMan <kieran9fowler@gmail.com>
2024-06-04 09:49:32 +0100

    return the output change Chunz Size to 160, and overlap to 15 delete reqfrom get function

--- Embedding.js
+++ Embedding.js
@@ -6,55 +6,40 @@ import readTxtFile from './readTxtFile.js';
 const movieTxt = readTxtFile('./movies.txt')
 const app = express()
 
-export default app.get('/getEmbedding',async(req,res) => {
+export default app.get('/getEmbedding',async(res) => {
 
       async function splitDocument(document) {
         const text = await document
         const splitter = new RecursiveCharacterTextSplitter({
-          chunkSize: 250,
-          chunkOverlap: 35,
+          chunkSize: 160,
+          chunkOverlap: 15,
         });
         const output = await splitter.createDocuments([text]);
-        return output 
+        return output;
       }
-
+   
       async function CreateAndStoreEmbeddings() {
-        const chunkData = await splitDocument(movieTxt)
-        const data = await Promise.all(
-          chunkData.map( async (chunk) => {
-              const embeddingResponse = await openai.embeddings.create({
-                  model: "text-embedding-ada-002",
-                  input: chunk.pageContent
-              });
-              return {
-                content: chunk.pageContent, 
-                embedding: embeddingResponse.data[0].embedding 
-              }
-          })
-        );
+              const chunkData = await splitDocument(movieTxt)
+              const data = await Promise.all(
+                chunkData.map( async (chunk) => { 
+                    const embeddingResponse = await openai.embeddings.create({
+                        model: "text-embedding-ada-002",
+                        input: chunk.pageContent
+                    });
+                        return {
+                          content: chunk.pageContent, 
+                          embedding: embeddingResponse.data[0].embedding 
+                        }
+                })
+              );
+      
+        //console.log(data)
         
         await supabase.from('movies').insert(data); 
         console.log('Embedding and storing complete!');
       }
 
       CreateAndStoreEmbeddings()
-})
-  
-            
-        
-
-
- 
-  // app.get('/getResponse',async(req,res) => {
-  //       //const userPrompt = req.body.userPrompt;
-  //       //console.log(userPrompt)
-  //       const reponse = await openai.chat.completions.create({
-  //         model:'gpt-3.5-turbo',
-  //         messages:[{"role":'user','content':'what is a rabbit?'}],
-  //         max_tokens:100
-  //       })
-  //       console.log(reponse.choices[0].message)
-  //     })
-      
+});
       
     
