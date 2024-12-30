97f111cab5728e97210aa08357f68707d4f4be60
KieranFowler <kieran9fowler@gmail.com>
2024-12-30 18:21:00 +0000

    formated document with prettier

---
+++ server/utils/readTxtFile.js
@@ -0,0 +1,12 @@
+import fs from 'node:fs'
+export default function readTxtFile(filePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
