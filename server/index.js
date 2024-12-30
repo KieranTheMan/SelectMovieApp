7a8cb2ea150aaeb75ec3647c8b03df9e4f88bb71
KieranFowler <kieran9fowler@gmail.com>
2024-12-30 18:15:21 +0000

    create new simple server file to replace server side rendering setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import getEmbedding from "./routes/Embedding.js";
+import query from "./routes/query.js";
+
+const app = express();
+app.use(cors());
+//app.use(express.json({ limit: "50mb" }));
+
+//TextSplit & Embedding MovieList and updates supabase database
+app.use("/", getEmbedding);
+//User Search Query & AI ....
+app.use("/", query);
+
+app.get("/", async (req, res) => {
+  res.send("Hello API Server");
+});
+
+const startServer = async () => {
+  try {
+    app.listen(8000, () =>
+      console.log("Server has started on port http://localhost:8000")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
