cf9e9058b272d13a37c1256c8409d2a18cb4c618
KieranFowler <kieran9fowler@gmail.com>
2024-12-31 21:19:10 +0000

    add bg-custom to display background image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <>
-      <main className="sm:p-8 px-4 py-8 w-full h-full bg-slate-800 min-h-[calc(100vh-73px)]">
+      <main className="bg-custom sm:p-8 px-4 py-8 w-full h-full bg-slate-800 min-h-[calc(100vh-73px)]">
         <Outlet />
       </main>
     </>
